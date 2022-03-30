55ddfb76c633a0afe789ecbef070c0e78f5728aa
Irene <irene.iaccio@gmail.com>
2022-03-30 13:51:36 +0200

    Fix eslint error

--- src/api/restGetPaymentDescriptor/restGetPaymentDescriptor.js
+++ src/api/restGetPaymentDescriptor/restGetPaymentDescriptor.js
@@ -1,7 +1,10 @@
 import RootElement from '../../../../../utils/rootElement';
 import sendRequest, { RESPONSE_JSON } from '../../../../../api/sendRequest';
 
-export default async function restGetPaymentDescriptor(appDispatch, checkoutSessionId) {
+export default async function restGetPaymentDescriptor(
+  appDispatch,
+  checkoutSessionId
+) {
   const paymentConfig = RootElement.getPaymentConfig();
   const url = `${paymentConfig.restUrlPrefix}amazon-checkout-session/${checkoutSessionId}/payment-descriptor`;
 
