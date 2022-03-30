4e9d5cb9dc229c858b13d435bec2f6480dc13b5b
Irene <irene.iaccio@gmail.com>
2022-03-30 12:33:10 +0200

    Fix get config and display button

--- src/api/restGetCheckoutSessionConfig/modifier.js
+++ src/api/restGetCheckoutSessionConfig/modifier.js
@@ -1,17 +1,22 @@
+import { _isObjEmpty } from '../../../../../utils';
+
 export default function restGetCheckoutSessionConfig(result) {
-  return {
-    merchantId: result[0],
-    ledgerCurrency: result[1],
-    buttonColor: result[2],
-    checkoutLanguage: result[3],
-    productType: result[4],
-    sandbox: result[5],
-    loginPayload: result[6],
-    loginSignature: result[7],
-    checkoutReviewPayload: result[8],
-    checkoutReviewSignature: result[9],
-    checkoutPayload: result[10],
-    checkoutSignature: result[11],
-    publicKeyId: result[12],
-  };
+  if (!_isObjEmpty(result)) {
+    return {
+      merchantId: result[0].merchant_id,
+      ledgerCurrency: result[0].currency,
+      buttonColor: result[0].button_color,
+      checkoutLanguage: result[0].language,
+      productType: false,
+      sandbox: result[0].sandbox,
+      loginPayload: result[0].login_payload,
+      loginSignature: result[0].login_signature,
+      checkoutReviewPayload: result[0].checkout_payload,
+      checkoutReviewSignature: result[0].checkout_signature,
+      checkoutPayload: result[0].paynow_payload,
+      checkoutSignature: result[0].paynow_signature,
+      publicKeyId: result[0].public_key_id,
+    };
+  }
+  return {};
 }
