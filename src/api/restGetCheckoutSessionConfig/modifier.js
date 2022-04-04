ee6405b6bc727e786d66c356a8ec883acc05ca84
Irene <irene.iaccio@gmail.com>
2022-04-04 09:43:54 +0200

    Implement suggestion: usa a const for sessionConfig

--- src/api/restGetCheckoutSessionConfig/modifier.js
+++ src/api/restGetCheckoutSessionConfig/modifier.js
@@ -2,20 +2,21 @@ import { _isObjEmpty } from '../../../../../utils';
 
 export default function restGetCheckoutSessionConfig(result) {
   if (!_isObjEmpty(result)) {
+    const sessionConfig = result[0] || {};
     return {
-      merchantId: result[0].merchant_id,
-      ledgerCurrency: result[0].currency,
-      buttonColor: result[0].button_color,
-      checkoutLanguage: result[0].language,
+      merchantId: sessionConfig.merchant_id,
+      ledgerCurrency: sessionConfig.currency,
+      buttonColor: sessionConfig.button_color,
+      checkoutLanguage: sessionConfig.language,
       productType: false,
-      sandbox: result[0].sandbox,
-      loginPayload: result[0].login_payload,
-      loginSignature: result[0].login_signature,
-      checkoutReviewPayload: result[0].checkout_payload,
-      checkoutReviewSignature: result[0].checkout_signature,
-      checkoutPayload: result[0].paynow_payload,
-      checkoutSignature: result[0].paynow_signature,
-      publicKeyId: result[0].public_key_id,
+      sandbox: sessionConfig.sandbox,
+      loginPayload: sessionConfig.login_payload,
+      loginSignature: sessionConfig.login_signature,
+      checkoutReviewPayload: sessionConfig.checkout_payload,
+      checkoutReviewSignature: sessionConfig.checkout_signature,
+      checkoutPayload: sessionConfig.paynow_payload,
+      checkoutSignature: sessionConfig.paynow_signature,
+      publicKeyId: sessionConfig.public_key_id,
     };
   }
   return {};
